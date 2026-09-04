d680c4a2060ab53596f52db9e519bd7ea26a8e9b
neeteshmehra2006-creator <neetesh.mehra2006@gmail.com>
2026-09-04 11:24:40 +0530

    Time: 0 ms (100%), Space: 42.5 MB (98%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+    for(char ch = 'a'; ch<='z'; ch++){
+        if(sentence.indexOf(ch)==-1)return false;
+    }
+    return true;
+    }
+}
