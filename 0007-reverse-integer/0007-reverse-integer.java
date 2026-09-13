8248b19a8ac895bec00e800b50df97259571a4dd
neeteshmehra2006-creator <neetesh.mehra2006@gmail.com>
2026-09-13 10:35:10 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int reverse(int x) {
+    int rev = 0;
+
+    while(x!=0){
+        int rem = x %10;
+        rev = rev*10+rem;
+        x = x/10;
+    }
+    return rev;
+    }
+}
