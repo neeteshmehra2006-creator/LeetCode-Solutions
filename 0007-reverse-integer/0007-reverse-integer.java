e2f4e691e392ca35e336eef32ffb158a4a5453a6
neeteshmehra2006-creator <neetesh.mehra2006@gmail.com>
2026-09-14 09:35:13 +0530

    Time: 1 ms (99.97%), Space: 42.7 MB (26.53%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,12 +1,14 @@
 class Solution {
     public int reverse(int x) {
-    int rev = 0;
-
-    while(x!=0){
-        int rem = x %10;
-        rev = rev*10+rem;
-        x = x/10;
-    }
-    return rev;
+     int   rev=0;
+        while(x!=0){
+            int digit=x%10;
+            //max limit of digit in 32 bit in java is 7 and minimum is -8
+            if(rev>Integer.MAX_VALUE/10||(rev==Integer.MAX_VALUE&&digit>7)) return 0;
+             if(rev<Integer.MIN_VALUE/10||(rev==Integer.MIN_VALUE&&digit<-8)) return 0;
+             rev=rev*10+digit;
+             x/=10;
+        }
+        return rev;
     }
 }
