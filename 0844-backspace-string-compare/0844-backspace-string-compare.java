daaf1b4f0d23aacc14a25d5c5e0b498254723a43
neeteshmehra2006-creator <neetesh.mehra2006@gmail.com>
2026-09-23 23:06:03 +0530

    Time: 5 ms (3.36%), Space: 43.6 MB (10.7%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        String a = "";
+        String b = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != '#') {
+                a = a + s.charAt(i);
+            } else if (a.length() > 0) {
+                a = a.substring(0, a.length() - 1);
+            }
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            if (t.charAt(i) != '#') {
+                b = b + t.charAt(i);
+            } else if (b.length() > 0) {
+                b = b.substring(0, b.length() - 1);
+            }
+        }
+        return a.equals(b);
+    }
+}
